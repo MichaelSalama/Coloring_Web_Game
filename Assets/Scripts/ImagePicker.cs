977fa886ea1daf37a8d43c4d3e827231f71cd8b7
= <moh_ussama@hotmail.com>
2018-12-13 09:07:22 +0200

    return to menu + reset to white + ui tweaks

--- Assets/Scripts/ImagePicker.cs
+++ Assets/Scripts/ImagePicker.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ImagePicker : MonoBehaviour {
+public class ImagePicker : MonoBehaviour
+{
 
     public IntVariable sketchNumber;
     public List<GameObject> sketchPrefabs;
 
-	
-	public void ShowSketch()
+
+    public void ShowSketch()
     {
         foreach (GameObject obj in sketchPrefabs)
         {
@@ -22,6 +23,20 @@ public class ImagePicker : MonoBehaviour {
         foreach (GameObject obj in sketchPrefabs)
         {
             obj.SetActive(false);
+        }
+    }
+
+    public void ResetToWhite()
+    {
+        foreach (GameObject obj in sketchPrefabs)
+        {
+            if (obj.activeInHierarchy)
+            {
+                for (int i = 0; i < obj.transform.childCount; i++)
+                {
+                    obj.GetComponent<Painting>().children[i].color = Color.white;
+                }
+            }
         }
     }
 }
---
+++ Assets/Scripts/Painting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Painting : MonoBehaviour
+{
+
+    public List<SpriteRenderer> children;
+
+    private void OnEnable()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            children.Add(transform.GetChild(i).GetComponent<SpriteRenderer>()); 
+        }
+        Debug.Log("transform.childCount "+ transform.childCount);
+
+        
+    }
+
+
+}
