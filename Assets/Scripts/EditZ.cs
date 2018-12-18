12c43be56a1e8c0011df7ec66fc2e87929bfe605
= <moh_ussama@hotmail.com>
2018-12-18 04:01:47 +0200

    Automated Art placement with Editor

---
+++ Assets/Scripts/EditZ.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditZ : MonoBehaviour {
+
+    
+    public void RandomSortingLayer()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).GetComponent<SpriteRenderer>().sortingOrder=(int) Random.Range(1,31);
+        }
+    }
+    public void SortingZ()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            float sortLayer = (float)transform.GetChild(i).GetComponent<SpriteRenderer>().sortingOrder;
+            transform.GetChild(i).transform.position = new Vector3(0, 0, -sortLayer / 10);
+        }
+    }
+    public void AddTag()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).transform.tag="Picture";
+        }
+    }
+    public void AddPaintingScript()
+    {
+        gameObject.AddComponent<Painting>();
+    }
+
+    public void AddPolygonCollider()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.AddComponent<PolygonCollider2D>();
+        }
+    }
+}
---
+++ Assets/Scripts/EditorSorter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(EditZ))]
+public class ObjectBuilderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        EditZ myScript = (EditZ)target;
+        if (GUILayout.Button("Sort"))
+        {
+            myScript.RandomSortingLayer();
+        }
+        if (GUILayout.Button("EditZ"))
+        {
+            myScript.SortingZ();
+        }
+        if (GUILayout.Button("AddTag"))
+        {
+            myScript.AddTag();
+        }
+        if (GUILayout.Button("AddScript"))
+        {
+            myScript.AddPaintingScript();
+        }
+        if (GUILayout.Button("AddPolygonCollider"))
+        {
+            myScript.AddPolygonCollider();
+        }
+    }
+}
