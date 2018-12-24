871f683dcc166c78b654c7cd9e8354fe792cb109
MichaelSalama <michael.fawzy91@yahoo.com>
2018-12-25 00:05:42 +0200

    Integrated Screen Recorder

--- Assets/Scripts/EditorSorter.cs
+++ Assets/Scripts/EditorSorter.cs
@@ -1,34 +1,34 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEditor;
+﻿//using UnityEngine;
+//using System.Collections;
+//using UnityEditor;
 
-[CustomEditor(typeof(EditZ))]
-public class ObjectBuilderEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        DrawDefaultInspector();
+//[CustomEditor(typeof(EditZ))]
+//public class ObjectBuilderEditor : Editor
+//{
+//    public override void OnInspectorGUI()
+//    {
+//        DrawDefaultInspector();
 
-        EditZ myScript = (EditZ)target;
-        if (GUILayout.Button("Sort"))
-        {
-            myScript.RandomSortingLayer();
-        }
-        if (GUILayout.Button("EditZ"))
-        {
-            myScript.SortingZ();
-        }
-        if (GUILayout.Button("AddTag"))
-        {
-            myScript.AddTag();
-        }
-        if (GUILayout.Button("AddScript"))
-        {
-            myScript.AddPaintingScript();
-        }
-        if (GUILayout.Button("AddPolygonCollider"))
-        {
-            myScript.AddPolygonCollider();
-        }
-    }
-}
+//        EditZ myScript = (EditZ)target;
+//        if (GUILayout.Button("Sort"))
+//        {
+//            myScript.RandomSortingLayer();
+//        }
+//        if (GUILayout.Button("EditZ"))
+//        {
+//            myScript.SortingZ();
+//        }
+//        if (GUILayout.Button("AddTag"))
+//        {
+//            myScript.AddTag();
+//        }
+//        if (GUILayout.Button("AddScript"))
+//        {
+//            myScript.AddPaintingScript();
+//        }
+//        if (GUILayout.Button("AddPolygonCollider"))
+//        {
+//            myScript.AddPolygonCollider();
+//        }
+//    }
+//}
