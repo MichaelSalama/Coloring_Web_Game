dcdfabb19e53abd7b4fbdcc2041a2f7d350a6a1d
MichaelSalama <michael.fawzy91@yahoo.com>
2018-12-26 16:55:16 +0200

    IntroScreenScript added & Start button adjusted

---
+++ Assets/Scripts/IntroScreenHandlerScript.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroScreenHandlerScript : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject startButton;
+    public GameObject socialGroup;
+
+    private void Start()
+    {
+        animator.SetBool("Scale", false);
+        startButton.SetActive(false);
+        socialGroup.SetActive(false);
+        StartCoroutine(WaitAnimation());
+    }
+
+    IEnumerator WaitAnimation()
+    {
+        Debug.Log("hena");
+        yield return new WaitForSecondsRealtime(3.5f);
+        animator.SetBool("Scale", true);
+        StartCoroutine(WaitScale());
+        Debug.Log("there");
+    }
+    IEnumerator WaitScale()
+    {
+        yield return new WaitForSecondsRealtime(1);
+        startButton.SetActive(true);
+        socialGroup.SetActive(true);
+    }
+}
