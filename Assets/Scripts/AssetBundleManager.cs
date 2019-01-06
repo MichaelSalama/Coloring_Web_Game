5ad99449b4f23b5d9e8410b448b3ce9f01dad7dd
Mohamed Hamdy <mohamedhamdy231995@gmail.com>
2019-01-06 04:58:56 +0200

    Make Asset Bundle Manager

---
+++ Assets/Scripts/AssetBundleManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class AssetBundleManager : MonoBehaviour
+{
+    public IntVariable sketchNumber;
+    public List<string> assetBundleNames;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    IEnumerator InstantiateObject()
+    {
+        string uri = "file:///" + Application.dataPath + "/AssetBundles/" + assetBundleNames[sketchNumber.value]+".txt";
+        UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(uri, 0);
+        yield return request.SendWebRequest();
+        if (request.isNetworkError || request.isHttpError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(request);
+
+
+            var ob = bundle.LoadAsset<GameObject>(assetBundleNames[sketchNumber.value]+".prefab");
+            Instantiate(ob);
+        }
+      
+    }
+}
